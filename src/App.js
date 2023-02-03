d576d4312d74eda216b7f8e57acf4b29596fa30c
Juan Pablo Larice <juanpablolarice@gmail.com>
2023-02-03 00:26:00 -0300

    Se crea componente NavBar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editá <code>src/App.js</code> y guardá para recargar.
         </p>
         <a
           className="App-link"
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NavBar from './components/NavBar/NavBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  	<React.StrictMode>
+    	{/* <App /> */}
+    	<NavBar/>
+  	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
