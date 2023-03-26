ddf46cb50fe2a923e57c2c4f328362f17ca3fe45
Juan Pablo Larice <juanpablolarice@gmail.com>
2023-03-26 15:32:54 -0300

    Se implementa el Order Form

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -9,6 +9,10 @@ import OrderForm from '../OrderForm/OrderForm';
 const Checkout = () => {
     const { cart, addToCart, removeFromCart, getTotalQuantity, getTotal } = useContext(CartContext)
     const results = [];
+    const [checkout, setCheckout] = useState(false)
+    const changeCheckout = (value) => {
+        setCheckout(value)
+    }
 
     cart.forEach((product, index) => {
         results.push(            
@@ -56,55 +60,70 @@ const Checkout = () => {
             </div>
         );
     })
-    
-    return (       
-        <>
-            <div className="container mt-3">
-                <div className="row">
-                    <div className="col-6 text-start px-0 py-2">
-                        <Link to={'/'} className="btn btn-continue-shopping capitalize-first">Continue Shopping</Link>
+
+    if(checkout){
+        return (       
+            <>
+                <div className="container mt-3">
+                    <div className="row">
+                        <div className="col-6 text-start px-0 py-2">
+                            <Link to={'/'} className="btn btn-continue-shopping capitalize-first">Continue Shopping</Link>
+                        </div>
                     </div>
-                    {/* <div className="col-6 text-end px-0 py-2">
-                        <a href="#" className="btn btn-checkout">Checkout</a>
-                    </div> */}
                 </div>
-            </div>
-            <div className="container bg-white shadow-lg mb-3">
-                {
-                    results.length > 0 ?
-                        <>                        
-                            <div className="row border-bottom mx-3">
-                                <div className="col-12 ps-0">
-                                    <h3 className="py-2">Shopping cart</h3>
-                                </div>
-                            </div>
-                            {results}
-                            <div className="row py-4">
-                                <div className="col-12">
-                                    <p className="text-end pe-4">
-                                        Items: {getTotalQuantity()}
-                                    </p>
-                                    <p className="text-end pe-4 pb-0">
-                                        Total: ${getTotal()}
-                                    </p>
-                                </div>
-                                <div className="col-12 text-end">
-                                    <a href="#" className="btn btn-checkout" data-bs-toggle="modal" data-bs-target="#orderFormModal">Checkout</a>
-                                </div>
-                            </div>
-                        </>
-                    :
-                    <div className="text-center text-body-tertiary py-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="160" height="160" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
-                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                        </svg>
-                        <h4 className="pt-2">Empty</h4>
+                <div className="container bg-white shadow-lg mb-3 text-center py-4" id="checkoutContainer">
+                    <h2 className='my-4'>Congratulations, your order was created correctly.</h2>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="160" height="160" fill="#198754" class="bi bi-truck" viewBox="0 0 16 16">
+                        <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                    </svg>
+                </div>    
+                <OrderForm changeCheckout={changeCheckout}/>            
+            </> 
+        )
+    }else{
+        return (       
+            <>
+                <div className="container mt-3">
+                    <div className="row">
+                        <div className="col-6 text-start px-0 py-2">
+                            <Link to={'/'} className="btn btn-continue-shopping capitalize-first">Continue Shopping</Link>
+                        </div>
                     </div>
-                }
-            </div>
-            <OrderForm/>
-        </> 
-    )
+                </div>
+                <div className="container bg-white shadow-lg mb-3" id="checkoutContainer">
+                    {                    
+                        results.length > 0 ?
+                            <>                        
+                                <div className="row border-bottom mx-3">
+                                    <div className="col-12 ps-0">
+                                        <h3 className="py-2">Shopping cart</h3>
+                                    </div>
+                                </div>
+                                {results}
+                                <div className="row py-4">
+                                    <div className="col-12">
+                                        <p className="text-end pe-4 pb-0">
+                                            Total <i>({getTotalQuantity()} items)</i>: <strong>${getTotal()}</strong>
+                                        </p>
+                                    </div>
+                                    <div className="col-12 text-end">
+                                        <a href="#" className="btn btn-checkout" data-bs-toggle="modal" data-bs-target="#orderFormModal">Checkout</a>
+                                    </div>
+                                </div>
+                            </>
+                        :
+                        <div className="text-center text-body-tertiary py-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="160" height="160" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
+                                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                            </svg>
+                            <h4 className="pt-2">Empty</h4>
+                        </div>
+                    }
+                </div>
+                <OrderForm changeCheckout={changeCheckout}/>
+            </> 
+        )
+    }     
 }
 
 export default Checkout
--- src/components/OrderForm/OrderForm.jsx
+++ src/components/OrderForm/OrderForm.jsx
@@ -1,36 +1,126 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { addDoc, collection, getFirestore } from 'firebase/firestore'
+import { CartContext } from '../../context/CartContext';
+import Loader from '../Loader/Loader';
 
-const OrderForm = () => {
-  return (
-    <div class="modal" id="orderFormModal" tabindex="-1">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Order form</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="mb-3">
-                        <label for="name" class="form-label">Name</label>
-                        <input type="text" class="form-control" id="name" placeholder="name" />
+const OrderForm = ({changeCheckout}) => {
+    const { cart, getTotal, clearCart } = useContext(CartContext)
+    const [buyerName, setBuyerName] = useState('');
+    const [buyerPhone, setBuyerPhone] = useState('');
+    const [buyerEmail, setBuyerEmail] = useState('');
+    const [total, setTotal] = useState(0);
+    const orderFormModal = document.getElementById('close-modal')
+    const checkoutContainer = document.getElementById('checkoutContainer')
+    const [isLoading, setIsLoading] = useState(false);
+
+    // Instancia de firestore
+    const db = getFirestore();
+
+    // Referencia a la colección ordes para acceder a dicha colección.
+    const ordersCollection = collection(db, 'orders');
+
+    // Función para manejar el submit del form
+    const handleSubmit = (e) => {
+        e.preventDefault(); // Evitamos que el formulario se envié por si mismo.
+        setIsLoading(true)
+  
+        // Objeto ‘order’ con la información del comprador, los ítems y su total.
+        const order = {
+            buyer: {
+                name: buyerName,
+                phone: buyerPhone,
+                email: buyerEmail
+            },
+            items: cart,
+            total: getTotal()
+        };
+
+        // console.log(selectedItems);
+        // Método ‘addDoc’ para agregar un nuevo documento con los datos de la orden. 
+        addDoc(ordersCollection, order)
+            .then((docRef) => {
+                document.getElementById('close-modal').click();
+                // console.log('Documento enviado. ID:', docRef.id);
+                // checkoutContainer.innerHTML = "";
+                // alert('¡Compra realizada con éxito!');
+                resetForm();
+                clearCart();
+                const msg = 'Congrats'
+                changeCheckout(true)
+                setIsLoading(false)
+                // document.getElementById("checkoutContainer").innerHTML = 'hasdasdasdtml';
+                // checkoutContainer.innerHTML('<h3>Congratulations, your order was created correctly.</h3>');
+            })
+            .catch((e) => {
+                console.log('Order fail', e);
+                setIsLoading(false)
+            });
+    };
+
+    // Función para manejar el seleccionado/deseleccionado de los checkbok
+    // const handleSelectItem = (item) => {
+    //     // Comprobamos si el elemento seleccionado está o no en la lista de elementos.
+    //     const isSelected = selectedItems.includes(item);
+
+    //     // Si no está seleccionado, se agrega a la lista de elementos y se agrega su precio al total.
+    //     if (!isSelected) {
+    //     setSelectedItems([...selectedItems, item]);
+    //     setTotal(total + item.price);
+    //     } else {
+    //     // Si el elemento ya está seleccionado, al deseleccionar se elimina de la lista y se resta su precio total al total (total).
+    //     const updatedSelectedItems = selectedItems.filter((selectedItem) => selectedItem.id !== item.id);
+    //     setSelectedItems(updatedSelectedItems);
+    //     setTotal(total - item.price);
+    //     }
+    // };
+
+    // Función para resetear el formulario luego de enviar
+    const resetForm = () => {
+        setBuyerName('');
+        setBuyerPhone('');
+        setBuyerEmail('');
+        // setSelectedItems([]);
+        setTotal(0);
+    };
+
+    return (
+        <div class="modal" id="orderFormModal" tabindex="-1">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Order form</h5>
+                        <button type="button" id="close-modal" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <div class="mb-3">
-                        <label for="email" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="email" placeholder="name@example.com" />
+                    <div class="modal-body">
+                        <form onSubmit={handleSubmit}>
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Name</label>
+                                <input type="text" class="form-control" id="name" value={buyerName} onChange={(e) => setBuyerName(e.target.value)} />
+                            </div>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="email" value={buyerEmail} onChange={(e) => setBuyerEmail(e.target.value)} />
+                            </div>
+                            <div class="mb-3">
+                                <label for="phone" class="form-label">Phone</label>
+                                <input type="text" class="form-control" id="phone" value={buyerPhone} onChange={(e) => setBuyerPhone(e.target.value)} />
+                            </div>
+                            <div className="row">
+                                <div className="col-6 text-start">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                                </div>
+                                <div className="col-6 text-end">
+                                    <button type="submit" class="btn btn-checkout">Proceed to checkout</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                    <div class="mb-3">
-                        <label for="phone" class="form-label">Phone</label>
-                        <input type="text" class="form-control" id="phone" placeholder="" />
+                    <div class="modal-footer">
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                    <button type="button" class="btn btn-checkout">Proceed to checkout</button>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default OrderForm
